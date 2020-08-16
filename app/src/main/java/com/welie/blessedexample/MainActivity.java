ceffaf84ad88694c65a53766dc55a39a95c6955e
Martijn van Welie <martijn.van.welie@philips.com>
2020-08-16 09:52:00 +0200

    Add check for Location Services to demo app

--- app/src/main/java/com/welie/blessedexample/MainActivity.java
+++ app/src/main/java/com/welie/blessedexample/MainActivity.java
@@ -1,12 +1,15 @@
 package com.welie.blessedexample;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Build;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,8 @@ import com.welie.blessed.BluetoothPeripheral;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,10 +50,15 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
-        if(hasPermissions()) {
-            initBluetoothHandler();
+        // Check if Location services are on because they are required to make scanning work
+        if(checkLocationServices()) {
+            // Check if the app has the right permissions
+            if (hasPermissions()) {
+                initBluetoothHandler();
+            }
         }
     }
+
     private void initBluetoothHandler()
     {
         BluetoothHandler.getInstance(getApplicationContext());
@@ -67,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             BluetoothPeripheral peripheral = getPeripheral(intent.getStringExtra("Peripheral"));
             BloodPressureMeasurement measurement = (BloodPressureMeasurement) intent.getSerializableExtra("BloodPressure");
-            if (measurement == null || peripheral == null) return;
+            if (measurement == null) return;
 
             measurementValue.setText(String.format(Locale.ENGLISH, "%.0f/%.0f %s, %.0f bpm\n%s\n\nfrom %s", measurement.systolic, measurement.diastolic, measurement.isMMHG ? "mmHg" : "kpa", measurement.pulseRate, dateFormat.format(measurement.timestamp), peripheral.getName()));
         }
@@ -78,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             BluetoothPeripheral peripheral = getPeripheral(intent.getStringExtra("Peripheral"));
             TemperatureMeasurement measurement = (TemperatureMeasurement) intent.getSerializableExtra("Temperature");
-            if (measurement == null || peripheral == null) return;
+            if (measurement == null) return;
 
             measurementValue.setText(String.format(Locale.ENGLISH, "%.1f %s (%s)\n%s\n\nfrom %s", measurement.temperatureValue, measurement.unit == TemperatureUnit.Celsius ? "celcius" : "fahrenheit", measurement.type, dateFormat.format(measurement.timestamp), peripheral.getName()));
         }
@@ -113,6 +123,45 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    private boolean areLocationServicesEnabled() {
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager == null) {
+            Timber.e("could not get location manager");
+            return false;
+        }
+
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        return isGpsEnabled || isNetworkEnabled;
+    }
+
+    private boolean checkLocationServices() {
+        if (!areLocationServicesEnabled()) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Location services are not enabled")
+                    .setMessage("Scanning for Bluetooth peripherals requires locations services to be enabled.") // Want to enable?
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // if this button is clicked, just close
+                            // the dialog box and do nothing
+                            dialog.cancel();
+                        }
+                    })
+                    .create()
+                    .show();
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
